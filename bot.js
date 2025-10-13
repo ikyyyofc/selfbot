f9ac8f4eae3f7d612cb512b5d2d4c9203a74dca7
ikyyyofc <dikivirtual@gmail.com>
2025-10-13 15:49:29 +0700

    update bot.js

--- bot.js
+++ bot.js
@@ -180,6 +180,7 @@ const connect = async () => {
         const isGroup = from.endsWith("@g.us");
         if (!isGroup && !m.key.fromMe) return;
         if (isGroup && m.key.participant !== sock.user.lid.split(":")[0] + "@lid") return;
+        if (!isGroup) console.log(m)
 
         let text =
             m.message?.conversation ||
