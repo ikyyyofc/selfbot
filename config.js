4a56627ecce15c60d2437b68ad18419ca43620c0
ikyyyofc <dikivirtual@gmail.com>
2025-10-15 19:23:38 +0700

    update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 // config.js
 export default {
     SESSION: "session",
-    PAIRING_NUMBER: "6287866255637", // Nomor WA bot tanpa +
     PAIRING_CODE: "IKYYSELF",
     PREFIX: [".", "!", "/"] // Multi prefix
 };
