6ac3c1277d714a37e0eed107f62f00cb7e3b090f
ikyyyofc <dikivirtual@gmail.com>
2025-10-16 22:07:05 +0700

    update plugins/play.js

--- plugins/play.js
+++ plugins/play.js
@@ -8,24 +8,21 @@ export default async function ({ sock, text, reply, m }) {
             )
         ).json();
         if (!proses.success) return reply("gagal jir");
-        reply(
-            {
-                audio: { url: proses.result.downloadUrl },
-                mimetype: "audio/mpeg",
-                fileName: `${proses.result.metadata.title}.mp3`,
-                contextInfo: {
-                    externalAdReply: {
-                        title: proses.result.metadata.title,
-                        body: `${proses.result.metadata.channel}`,
-                        thumbnailUrl: proses.result.metadata.cover,
-                        mediaType: 1,
-                        showAdAttribution: false,
-                        renderlargerThumbnail: true
-                    }
+        reply({
+            audio: { url: proses.result.downloadUrl },
+            mimetype: "audio/mpeg",
+            fileName: `${proses.result.metadata.title}.mp3`,
+            contextInfo: {
+                externalAdReply: {
+                    title: proses.result.metadata.title,
+                    body: `${proses.result.metadata.channel}`,
+                    thumbnailUrl: proses.result.metadata.cover,
+                    mediaType: 1,
+                    showAdAttribution: false,
+                    renderlargerThumbnail: true
                 }
-            },
-            { quoted: m }
-        );
+            }
+        });
     } catch {
         reply("ntahlah error pluginnya");
     }
