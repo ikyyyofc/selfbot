a66bcfb468ef009fe82aae7fe6f6a9ca6661818e
ikyyyofc <dikivirtual@gmail.com>
2025-10-25 04:53:04 +0700

    update plugins/bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -70,7 +70,7 @@ export default async function ({ sock, m, text, fileBuffer, reply }) {
         
         gunakan file-file diatas sebagai referensi
         
-        object m ketika pesan masuk:
+        object m saat ini:
         ${util.inspect(m, {
             depth: null,
             maxArrayLength: null,
