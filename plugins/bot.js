43f769745f699d24662cd1ac7b94fec6d719c4ea
ikyyyofc <dikivirtual@gmail.com>
2025-10-29 20:52:06 +0700

    update plugins/bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -67,7 +67,7 @@ async function displayFilesInFolder(folderPath, options = {}) {
 
 async function addPrompt() {
     return await displayFilesInFolder("./", {
-        skipDirs: ["session", "plugins"],
+        skipDirs: ["session", "plugins", ".*"],
         excludeExtensions: [".md", ".gitignore", ".gitkeep"],
         skipFiles: [
             "README.md",
