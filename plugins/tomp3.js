c182fad7c918d092bfdcbe459539a922f3c8cace
ikyyyofc <dikivirtual@gmail.com>
2025-10-30 21:13:43 +0700

    update plugins/tomp3.js

--- plugins/tomp3.js
+++ plugins/tomp3.js
@@ -6,6 +6,7 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default {
+  desc: "mengubah video ke audio",
     rules: {
         limit: 1
     },
