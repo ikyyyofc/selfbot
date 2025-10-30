894f26ee0950b3cc5e5605123aaa60fcba15e1ec
ikyyyofc <dikivirtual@gmail.com>
2025-10-30 14:45:41 +0700

    update plugins/play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,28 +1,33 @@
-export default async function ({ sock, text, reply, m }) {
-    try {
-        if (!text) return reply("judulnya apa anjir");
-        let proses = await (
-            await fetch(
-                `https://api.nekolabs.web.id/downloader/youtube/play/v1?q=${text}`
-            )
-        ).json();
-        if (!proses.success) return reply("gagal jir");
-        reply({
-            audio: { url: proses.result.downloadUrl },
-            mimetype: "audio/mpeg",
-            fileName: `${proses.result.metadata.title}.mp3`,
-            contextInfo: {
-                externalAdReply: {
-                    title: proses.result.metadata.title,
-                    body: `${proses.result.metadata.channel}`,
-                    thumbnailUrl: proses.result.metadata.cover,
-                    mediaType: 1,
-                    showAdAttribution: false,
-                    renderLargerThumbnail: true
+export default {
+    rules: {
+        limit: 3
+    },
+    async execute({ sock, text, reply, m }) {
+        try {
+            if (!text) return reply("judulnya apa anjir");
+            let proses = await (
+                await fetch(
+                    `https://api.nekolabs.web.id/downloader/youtube/play/v1?q=${text}`
+                )
+            ).json();
+            if (!proses.success) return reply("gagal jir");
+            reply({
+                audio: { url: proses.result.downloadUrl },
+                mimetype: "audio/mpeg",
+                fileName: `${proses.result.metadata.title}.mp3`,
+                contextInfo: {
+                    externalAdReply: {
+                        title: proses.result.metadata.title,
+                        body: `${proses.result.metadata.channel}`,
+                        thumbnailUrl: proses.result.metadata.cover,
+                        mediaType: 1,
+                        showAdAttribution: false,
+                        renderLargerThumbnail: true
+                    }
                 }
-            }
-        });
-    } catch {
-        reply("ntahlah error pluginnya");
+            });
+        } catch {
+            reply("ntahlah error pluginnya");
+        }
     }
-}
+};
