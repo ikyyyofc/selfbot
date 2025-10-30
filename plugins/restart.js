89cb7122a224bcdcd575c69586261358b7a1a3db
ikyyyofc <dikivirtual@gmail.com>
2025-10-30 14:50:29 +0700

    update plugins/restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -1,8 +1,13 @@
-export default async function ({ reply }) {
-  try {
-    await reply("🔄 Restarting bot...");
-    process.exit(0);
-  } catch (e) {
-    reply(`❌ Gagal restart: ${e.message}`);
-  }
-}
+export default {
+    rules: {
+        owner: true
+    },
+    async function({ reply }) {
+        try {
+            await reply("🔄 Restarting bot...");
+            process.exit(0);
+        } catch (e) {
+            reply(`❌ Gagal restart: ${e.message}`);
+        }
+    }
+};
