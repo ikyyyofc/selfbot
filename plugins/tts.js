bb916ac67684f2ed9af1d8897556955384d74ed5
ikyyyofc <dikivirtual@gmail.com>
2025-12-07 15:05:30 +0700

    update plugins/tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -34,7 +34,7 @@ async function tts(
                 parts: [
                     {
                         text:
-                            "[selalu gunakan gaya bicara yang imut dan gemesin. selalu gunakan nada lemas, lelah, merintih birahi, dan seolah-olah mendesah seperti sedang atau setelah melakukan hubungan seksual]: " +
+                            "[selalu gunakan gaya bicara yang imut dan gemesin. selalu gunakan nada lemas, lelah, dan merintih birahi seperti sedang atau setelah melakukan hubungan seksual]: " +
                             text
                     }
                 ]
