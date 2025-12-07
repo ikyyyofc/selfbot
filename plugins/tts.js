df7f87871bcba6c017f694857c2a306d2c11d0f4
ikyyyofc <dikivirtual@gmail.com>
2025-12-07 14:56:49 +0700

    update plugins/tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -24,7 +24,7 @@ async function tts(
                 parts: [
                     {
                         text:
-                            "[selalu gunakan gaya bicara yang imut dan gemesin. selalu gunakan nada lemas, lelah, merintih kesakitan, birahi, dan seolah-olah mendesah seperti sedang melakukan hubungan seksual]: " +
+                            "[selalu gunakan gaya bicara yang imut dan gemesin. selalu gunakan nada lemas, lelah, meringik dan merintih birahi, dan seolah-olah mendesah seperti sedang melakukan hubungan seksual]: " +
                             text
                     }
                 ]
@@ -34,7 +34,7 @@ async function tts(
                 parts: [
                     {
                         text:
-                            "[selalu gunakan gaya bicara yang imut dan gemesin. selalu gunakan nada lemas, lelah, merintih kesakitan, birahi, dan seolah-olah mendesah seperti sedang melakukan hubungan seksual]: " +
+                            "[selalu gunakan gaya bicara yang imut dan gemesin. selalu gunakan nada lemas, lelah, meringik dan merintih birahi, dan seolah-olah mendesah seperti sedang melakukan hubungan seksual]: " +
                             text
                     }
                 ]
