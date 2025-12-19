e9df117e7aa5006db4c4db22f948f79066785108
ikyyyofc <dikivirtual@gmail.com>
2025-12-20 00:15:38 +0700

    update plugins/ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,9 +1,8 @@
 export default {
-    async execute({ m, reply }) {
-        const start = m.timestamp;
-        const end = Date.now();
-        const latency = end - start;
-
-        await reply(`pong!\nkecepatan respon: ${latency} ms`);
-    }
+  async execute({ m, reply }) {
+    const startTime = m.timestamp;
+    const speed = Date.now() - startTime * 1000;
+    
+    await reply(`pong! ${speed} milidetik`);
+  }
 };
