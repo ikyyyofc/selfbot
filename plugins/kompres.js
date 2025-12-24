44dcd64437af9fcea4d791f4ab8598a768765e1c
ikyyyofc <dikivirtual@gmail.com>
2025-12-24 20:38:09 +0700

    update plugins/kompres.js

--- plugins/kompres.js
+++ plugins/kompres.js
@@ -44,7 +44,7 @@ export default {
         const { m, sock, text } = context;
 
         // cek ada video yg direply apa kaga
-        if (!m.quoted || !m.quoted.isMedia || !/video/.test(m.quoted.type)) {
+        if (!m.quoted || !m.quoted.isMedia)) {
             return m.reply("mana videonya anjg, reply video yg mau lu kompres pake command .kompres");
         }
 
