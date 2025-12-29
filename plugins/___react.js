08ac8b96f71d30f117d68cf76d2fcb7cf425cf2d
ikyyyofc <dikivirtual@gmail.com>
2025-12-29 19:50:03 +0700

    update plugins/___react.js

--- plugins/___react.js
+++ plugins/___react.js
@@ -1,7 +1,11 @@
+const target_number = "6283150413582"
+
 export default {
-    async execute({ m }) {
-        if (m.key.participantjid === "6283150413582@s.whatsapp.net") {
-            await m.react("😂");
+    async execute(context) {
+        const { m } = context
+        
+        if (m.sender.startsWith(target_number)) {
+            await m.react("😂")
         }
     }
-};
+}
