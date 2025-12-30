84d500c8e6ed162df23cd9e6b3531525d8e34ac3
ikyyyofc <dikivirtual@gmail.com>
2025-12-30 08:46:38 +0700

    update config.js

--- config.js
+++ config.js
@@ -16,8 +16,8 @@ export default {
     BOT_MODE: process.env.mode || "self",
 
     PREMIUM_GROUPS: [],
-    
-    AUTO_REACT_NUMBERS: ["6283150413582"] 
+
+    AUTO_REACT_NUMBERS: ["6283150413582"]
 };
 
 global.delay = ms => new Promise(resolve => setTimeout(resolve, ms));
