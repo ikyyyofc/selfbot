1d1916c110e7b94a9f9ba966eabe614a85c0ba87
ikyyyofc <dikivirtual@gmail.com>
2026-01-26 09:18:24 +0700

    update plugins/sc.js

---
+++ plugins/sc.js
@@ -0,0 +1,12 @@
+export default {
+    execute: async ({ m }) => {
+        await m.react("👾")
+        await m.reply(`*IKYY BASE SOURCE CODE*
+
+Nih sc-nya, gratis kok santai aja. Jangan lupa kasih star di GitHub-nya ye, biar gue semangat update.
+
+🔗 https://github.com/ikyyyofc/selfbot
+
+Kalau ada error, ya usaha dikit lah fix sendiri, jangan manja. 😹`)
+    }
+}
